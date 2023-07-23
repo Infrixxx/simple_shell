266c33ec681a337f66e3358bba8835c87c2c5567
Boitumelo <bcrachoshi@gmail.com>
2023-07-23 06:58:42 +0200

    implement functions

--- shell.c
+++ shell.c
@@ -1,27 +1,79 @@
+
 #include "shell.h"
+
 /**
- * main - Entry point for the Simple Shell program.
- * Return:EXIT_SUCCESS (0) to indicate successful termination.
+ * execute_command - Execute the given command
+ * @command: The command to execute
  */
-
-int main(void)
+void execute_command(char *command)
 {
-	 char *command;
+    char **args = tokenize_command(command);
 
-    while (1)
+    if (!args)
     {
-        display_prompt();
-        command = read_command();
-
-        if (!command)
-        {
-            printf("\n");
-            break;
-        }
-
-        execute_command(command);
-        free(command);
+        perror("Memory Allocation Error");
+        return;
     }
 
-    return (EXIT_SUCCESS);
+    if (check_executable(args[0]))
+    {
+        execute_command_with_args(args);
+    }
+
+    free(args);
+}
+
+/**
+ * tokenize_command - Tokenize the command into arguments
+ * @command: The command to tokenize
+ *
+ * Return: A NULL-terminated array of strings containing the arguments.
+ *         NULL if memory allocation fails.
+ */
+char **tokenize_command(char *command)
+{
+    char **args = malloc(BUFFER_SIZE * sizeof(char *));
+    int i = 0;
+
+    if (!args)
+    {
+        return NULL;
+    }
+
+    char *token = strtok(command, " ");
+    while (token != NULL)
+    {
+        args[i] = token;
+        i++;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL; /* Set the last element of the args array to NULL */
+
+    return args;
+}
+
+/**
+ * execute_command_with_args - Execute the command with arguments
+ * @args: The array of strings containing the command and its arguments
+ */
+void execute_command_with_args(char **args)
+{
+    pid_t pid = fork();
+
+    if (pid == 0) /* Child process */
+    {
+        if (execve(args[0], args, environ) == -1)
+        {
+            perror("Error");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else if (pid < 0)
+    {
+        perror("Fork Error");
+    }
+    else
+    {
+        wait(NULL); /* Parent process waits for child to complete */
+    }
 }
