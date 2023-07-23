e88a5217ce1378ca373acadaab1cf56e9f8baa80
Boitumelo <bcrachoshi@gmail.com>
2023-07-23 06:41:03 +0200

    split functions into files

---
+++ execute_command_with_args.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+
+/**
+* execute_command_with_args - Execute the command with arguments
+* @args: The array of strings containing the command and its arguments
+*/
+void execute_command_with_args(char **args)
+{
+    pid_t pid = fork();
+
+    if (pid == 0) /* Child process */
+    {
+        if (execve(args[0], args, environ) == -1)
+        {
+            perror("Error");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else if (pid < 0)
+    {
+        perror("Fork Error");
+    }
+    else
+    {
+        wait(NULL); /* Parent process waits for child to complete */
+    }
+}
