179e3fa39cdea5a1a1f4dbc987f84b24edd0b197
Boitumelo <bcrachoshi@gmail.com>
2023-07-23 09:56:08 +0200

    Implement the env built-in, that prints the current environment

---
+++ env_builtin.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * env_builtin - Print the current environment variables.
+ */
+void env_builtin(void)
+{
+	int i = 0;
+	while (environ[i] != NULL)
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+}
