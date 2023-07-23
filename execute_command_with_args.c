7d2799ef695bdadaf9dad20ef1ddd2caef5580e6
Boitumelo <bcrachoshi@gmail.com>
2023-07-23 07:19:06 +0200

    fix for betty

--- execute_command_with_args.c
+++ execute_command_with_args.c
@@ -7,22 +7,22 @@
 
 void execute_command_with_args(char **args)
 {
-pid_t pid = fork();
+	pid_t pid = fork();
 
-if (pid == 0) /* Child process */
-{
-if (execve(args[0], args, environ) == -1)
-{
-perror("Error");
-exit(EXIT_FAILURE);
-}
-}
-else if (pid < 0)
-{
-perror("Fork Error");
-}
-else
-{
-wait(NULL); /* Parent process waits for child to complete */
-}
+	if (pid == 0)
+	{
+		if (execve(args[0], args, environ) == -1)
+		{
+			perror("Error");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (pid < 0)
+	{
+		perror("Fork Error");
+	}
+	else
+	{
+		wait(NULL);
+	}
 }
