80bd20ae9370f4922955d634c03dcc87d5cf6875
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-07-23 07:09:52 +0200

    betty fix

--- execute_command.c
+++ execute_command.c
@@ -8,16 +8,15 @@
 void execute_command(char *command)
 {
     char **args = tokenize_command(command);
-
-if (!args)
-{
-perror("Memory Allocation Error");
-return;
-}
-
-if (check_executable(args[0]))
-{
-execute_command_with_args(args);
-}
-free(args);
+    if (!args)
+    {
+        perror("Memory Allocation Error");
+        return;
+    }
+    
+    if (check_executable(args[0]))
+    {
+        execute_command_with_args(args);
+    }
+    free(args);
 }
