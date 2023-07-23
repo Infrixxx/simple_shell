672701c273fd0ccd50cf114e4ab029da3f5f6f16
Boitumelo <bcrachoshi@gmail.com>
2023-07-23 10:55:34 +0200

    fix to custom

--- custom_getline.c
+++ custom_getline.c
@@ -9,59 +9,56 @@
 
 char *custom_getline(void)
 {
-static char buffer[BUFFER_SIZE];
-static int buffer_index = 0;
-static int characters_read = 0;
+	static char buffer[BUFFER_SIZE];
+	static int buffer_index = 0;
+	static int characters_read = 0;
+	char *line = NULL;
+	int i = 0;
 
-char *line = NULL;
-int i = 0;
+	if (characters_read == 0)
+	{
+		characters_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+		buffer_index = 0;
 
-if (characters_read == 0)
-{
-characters_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
-buffer_index = 0;
+	if (characters_read <= 0)
+	{
+		return NULL;
+	}
+	}
 
-if (characters_read <= 0)
-{
-return NULL;
-}
-}
+	line = malloc(BUFFER_SIZE * sizeof(char));
+	if (!line)
+	{
+		perror("Memory Allocation Error");
+		return NULL;
+	}
 
-line = malloc(BUFFER_SIZE * sizeof(char));
-if (!line)
-{
-perror("Memory Allocation Error");
-return NULL;
-}
-
-while (buffer_index < characters_read)
-{
-if (buffer[buffer_index] == '\n')
-{
-line[i] = '\0'; // Null-terminate the line
-buffer_index++;
-return line; // Return the line read
-}
-line[i] = buffer[buffer_index];
-i++;
-buffer_index++;
-
-if (i >= BUFFER_SIZE)
-{
-line = realloc(line, (i + BUFFER_SIZE) * sizeof(char));
-if (!line)
-{
-perror("Memory Allocation Error");
-return NULL;
-}
-}
-}
-
-if (buffer_index >= characters_read)
-{
-characters_read = 0;
-buffer_index = 0;
-}
-
-return line; 
+	while (buffer_index < characters_read)
+	{
+		if (buffer[buffer_index] == '\n')
+	{
+		line[i] = '\0';
+		buffer_index++;
+		return line;
+	}
+		line[i] = buffer[buffer_index];
+		i++;
+		buffer_index++;
+		
+		if (i >= BUFFER_SIZE)
+		{
+			line = realloc(line, (i + BUFFER_SIZE) * sizeof(char));
+			if (!line)
+			{
+				perror("Memory Allocation Error");
+				return NULL;
+			}
+		}
+	}
+	if (buffer_index >= characters_read)
+	{
+		characters_read = 0;
+		buffer_index = 0;
+	}
+	return line; 
 }
