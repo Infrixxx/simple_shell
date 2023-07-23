f70fbc0700ed2ce361b7672a9613bc095841b4b1
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-07-23 10:38:30 +0200

    Update custom_getline.c

--- custom_getline.c
+++ custom_getline.c
@@ -1,62 +1,67 @@
 #include "shell.h"
 
 #define BUFFER_SIZE 1024
+/**
+*custom_getline - Read a line of input from the standard input.
+* Return: On success, returns a pointer to the line read from input.
+*         On failure or end-of-file, returns NULL.
+*/
 
 char *custom_getline(void)
 {
-    static char buffer[BUFFER_SIZE];
-    static int buffer_index = 0;
-    static int characters_read = 0;
+static char buffer[BUFFER_SIZE];
+static int buffer_index = 0;
+static int characters_read = 0;
 
-    char *line = NULL;
-    int i = 0;
+char *line = NULL;
+int i = 0;
 
-    if (characters_read == 0)
-    {
-        characters_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
-        buffer_index = 0;
+if (characters_read == 0)
+{
+characters_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+buffer_index = 0;
 
-        if (characters_read <= 0)
-        {
-            return NULL;
-        }
-    }
+if (characters_read <= 0)
+{
+return NULL;
+}
+}
 
-    line = malloc(BUFFER_SIZE * sizeof(char));
-    if (!line)
-    {
-        perror("Memory Allocation Error");
-        return NULL;
-    }
+line = malloc(BUFFER_SIZE * sizeof(char));
+if (!line)
+{
+perror("Memory Allocation Error");
+return NULL;
+}
 
-    while (buffer_index < characters_read)
-    {
-        if (buffer[buffer_index] == '\n')
-        {
-            line[i] = '\0'; // Null-terminate the line
-            buffer_index++;
-            return line; // Return the line read
-        }
-        line[i] = buffer[buffer_index];
-        i++;
-        buffer_index++;
+while (buffer_index < characters_read)
+{
+if (buffer[buffer_index] == '\n')
+{
+line[i] = '\0'; // Null-terminate the line
+buffer_index++;
+return line; // Return the line read
+}
+line[i] = buffer[buffer_index];
+i++;
+buffer_index++;
 
-        if (i >= BUFFER_SIZE)
-        {
-            line = realloc(line, (i + BUFFER_SIZE) * sizeof(char));
-            if (!line)
-            {
-                perror("Memory Allocation Error");
-                return NULL;
-            }
-        }
-    }
+if (i >= BUFFER_SIZE)
+{
+line = realloc(line, (i + BUFFER_SIZE) * sizeof(char));
+if (!line)
+{
+perror("Memory Allocation Error");
+return NULL;
+}
+}
+}
 
-    if (buffer_index >= characters_read)
-    {
-        characters_read = 0;
-        buffer_index = 0;
-    }
+if (buffer_index >= characters_read)
+{
+characters_read = 0;
+buffer_index = 0;
+}
 
-    return line; 
+return line; 
 }
