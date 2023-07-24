79d8267e9b7f5323962636641b68059b2c337a57
Boitumelo <bcrachoshi@gmail.com>
2023-07-24 06:58:59 -0700

    fiox to betty on files builtin

--- builtins.c
+++ builtins.c
@@ -8,19 +8,19 @@
  */
 int setenv_builtin(char **args)
 {
-        if (args[1] == NULL || args[2] == NULL)
-        {
-                fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
-                return (-1);
-        }
+	if (args[1] == NULL || args[2] == NULL)
+	{
+		fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
+		return (-1);
+	}
 
-        if (setenv(args[1], args[2], 1) != 0)
-        {
-                perror("setenv");
-                return (-1);
-        }
+	if (setenv(args[1], args[2], 1) != 0)
+	{
+		perror("setenv");
+		return (-1);
+	}
 
-        return (1);
+	return (1);
 }
 
 /**
@@ -31,17 +31,17 @@ int setenv_builtin(char **args)
  */
 int unsetenv_builtin(char **args)
 {
-        if (args[1] == NULL)
-        {
-                fprintf(stderr, "Usage: unsetenv VARIABLE\n");
-                return (-1);
-        }
+	if (args[1] == NULL)
+	{
+		fprintf(stderr, "Usage: unsetenv VARIABLE\n");
+		return (-1);
+	}
 
-        if (unsetenv(args[1]) != 0)
-        {
-                perror("unsetenv");
-                return (-1);
-        }
+	if (unsetenv(args[1]) != 0)
+	{
+		perror("unsetenv");
+		return (-1);
+	}
 
-        return (1);
+	return (1);
 }
--- execute_command.c
+++ execute_command.c
@@ -8,36 +8,36 @@ void execute_command(char *command)
 {
 	char **args = tokenize_command(command);
 
-    if (!args)
-    {
-        perror("Memory Allocation Error");
-        return;
-    }
+	if (!args)
+	{
+		perror("Memory Allocation Error");
+		return;
+	}
 
-    /* Check if the command is a built-in and execute it */
-    if (execute_builtin(args))
-    {
-        free_arguments(args);
-        return;
-    }
+	/* Check if the command is a built-in and execute it */
+	if (execute_builtin(args))
+	{
+		free_arguments(args);
+		return;
+	}
 
-    if (strcmp(args[0], "exit") == 0)
-    {
-        int status = 0;
+	if (strcmp(args[0], "exit") == 0)
+	{
+		int status = 0;
 
-        if (args[1])
-        {
-            status = custom_atoi(args[1]);
-        }
+		if (args[1])
+		{
+			status = custom_atoi(args[1]);
+		}
 
-        free_arguments(args);
-        exit(status);
-    }
+		free_arguments(args);
+		exit(status);
+	}
 
-    if (check_executable(args[0]))
-    {
-        execute_command_with_args(args);
-    }
+	if (check_executable(args[0]))
+	{
+		execute_command_with_args(args);
+	}
 
-    free_arguments(args);
+	free_arguments(args);
 }
