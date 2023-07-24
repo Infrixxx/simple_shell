7e9631d7209fbc87542d07e40bb91e658399bc4b
Boitumelo <bcrachoshi@gmail.com>
2023-07-24 13:17:16 +0200

    8. Simple shell 0.4.1

--- execute_command.c
+++ execute_command.c
@@ -4,25 +4,33 @@
  * execute_command - Execute the given command
  * @command: The command to execute
  */
-
 void execute_command(char *command)
 {
-	char **args = tokenize_command(command);
+        char **args = tokenize_command(command);
 
-	if (!args)
-	{
-		perror("Memory Allocation Error");
-		return;
-	}
-	if (strcmp(args[0], "exit") == 0)
-	{
-		free(args);
-		exit(EXIT_SUCCESS);
-	}
-	if (check_executable(args[0]))
-	{
-		execute_command_with_args(args);
-	}
-	execute_command_with_args(args);
-	free(args);
+        if (!args)
+        {
+                perror("Memory Allocation Error");
+                return;
+        }
+
+        if (strcmp(args[0], "exit") == 0)
+        {
+                int status = 0;
+
+                if (args[1])
+                {
+                        status = custom_atoi(args[1]);
+                }
+
+                free_arguments(args);
+                exit(status);
+        }
+
+        if (check_executable(args[0]))
+        {
+                execute_command_with_args(args);
+        }
+
+        free_arguments(args);
 }
