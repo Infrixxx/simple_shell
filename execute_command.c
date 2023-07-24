80a32dd59b81d72217660ee80639cce2356f56e0
Boitumelo <bcrachoshi@gmail.com>
2023-07-24 15:29:25 +0200

    fix to execute command

--- execute_command.c
+++ execute_command.c
@@ -8,29 +8,36 @@ void execute_command(char *command)
 {
 	char **args = tokenize_command(command);
 
-	if (!args)
-	{
-		perror("Memory Allocation Error");
-		return;
-	}
+    if (!args)
+    {
+        perror("Memory Allocation Error");
+        return;
+    }
 
-	if (strcmp(args[0], "exit") == 0)
-	{
-		int status = 0;
+    /* Check if the command is a built-in and execute it */
+    if (execute_builtin(args))
+    {
+        free_arguments(args);
+        return;
+    }
 
-		if (args[1])
-		{
-			status = custom_atoi(args[1]);
-		}
+    if (strcmp(args[0], "exit") == 0)
+    {
+        int status = 0;
 
-		free_arguments(args);
-		exit(status);
-	}
+        if (args[1])
+        {
+            status = custom_atoi(args[1]);
+        }
 
-	if (check_executable(args[0]))
-	{
-		execute_command_with_args(args);
-	}
+        free_arguments(args);
+        exit(status);
+    }
 
-	free_arguments(args);
+    if (check_executable(args[0]))
+    {
+        execute_command_with_args(args);
+    }
+
+    free_arguments(args);
 }
