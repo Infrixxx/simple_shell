106888edf0addc51885ceb0611b4e4365a2c7978
Boitumelo <bcrachoshi@gmail.com>
2023-07-25 00:22:41 -0700

    creation of custom cd function

--- builtins1.c
+++ builtins1.c
@@ -9,29 +9,64 @@
 int execute_builtin(char **args)
 {
 	if (args[0] == NULL)
-		return 0;
+		return (0);
 
 	if (strcmp(args[0], "exit") == 0)
 	{
-		// Add your implementation for the exit command here
-		// ...
+		/* Add your implementation for the exit command here */
+		/* ... */
+		return (1);
 	}
 	else if (strcmp(args[0], "cd") == 0)
 	{
-		return cd_builtin(args);
+		return (cd_builtin(args));
 	}
 	else if (strcmp(args[0], "setenv") == 0)
 	{
-		return setenv_builtin(args);
+		return (setenv_builtin(args));
 	}
 	else if (strcmp(args[0], "unsetenv") == 0)
 	{
-		return unsetenv_builtin(args);
+		return (unsetenv_builtin(args));
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
+}
+
+/**
+ * get_home_directory - Get the HOME directory.
+ *
+ * Return: The HOME directory path or NULL if not found.
+ */
+static char *get_home_directory(void)
+{
+	char *home = getenv("HOME");
+
+	if (!home)
+	{
+		fprintf(stderr, "cd: No HOME environment variable.\n");
+		return (NULL);
+	}
+	return (home);
+}
+
+/**
+ * get_oldpwd_directory - Get the OLDPWD directory.
+ *
+ * Return: The OLDPWD directory path or NULL if not found.
+ */
+static char *get_oldpwd_directory(void)
+{
+	char *oldpwd = getenv("OLDPWD");
+
+	if (!oldpwd)
+	{
+		fprintf(stderr, "cd: No OLDPWD environment variable.\n");
+		return (NULL);
+	}
+	return (oldpwd);
 }
 
 /**
@@ -42,31 +77,16 @@ int execute_builtin(char **args)
  */
 int cd_builtin(char **args)
 {
-	char *dir;
+	char *dir = args[1];
 	char cwd[LINE_BUFFER_SIZE];
 
-	if (args[1] == NULL)
-	{
-		dir = getenv("HOME");
-		if (!dir)
-		{
-			fprintf(stderr, "cd: No HOME environment variable.\n");
-			return (-1);
-		}
-	}
-	else if (strcmp(args[1], "-") == 0)
-	{
-		dir = getenv("OLDPWD");
-		if (!dir)
-		{
-			fprintf(stderr, "cd: No OLDPWD environment variable.\n");
-			return (-1);
-		}
-	}
-	else
-	{
-		dir = args[1];
-	}
+	if (dir == NULL)
+		dir = get_home_directory();
+	else if (strcmp(dir, "-") == 0)
+		dir = get_oldpwd_directory();
+
+	if (!dir)
+		return (-1);
 
 	if (chdir(dir) != 0)
 	{
