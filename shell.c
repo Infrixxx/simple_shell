dc373fa88cdad794f9697cd1da9d63eca66275e2
Boitumelo <bcrachoshi@gmail.com>
2023-07-25 09:09:56 -0700

    function refractoring

--- shell.c
+++ shell.c
@@ -72,6 +72,7 @@ void execute_command_with_args(char **args)
 	if (pid == 0)
 	{
 		char **replaced_args = malloc((custom_strlen(args) + 1) * sizeof(char *));
+
 		if (!replaced_args)
 		{
 			perror("Memory Allocation Error");
